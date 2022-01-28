1fb489698d4a4cdd0bba1becc25f91afb8fb91c2
MPL <marcin.labenski@gmail.com>
2022-01-28 10:25:30 +0100

    Initial Commit

---
+++ mlft/mlft.h
@@ -0,0 +1,18 @@
+//
+//  mlft.h
+//  mlft
+//
+//  Created by Marcin Labenski on 28/01/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for mlft.
+FOUNDATION_EXPORT double mlftVersionNumber;
+
+//! Project version string for mlft.
+FOUNDATION_EXPORT const unsigned char mlftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <mlft/PublicHeader.h>
+
+
