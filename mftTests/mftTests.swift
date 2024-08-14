82b4795212d163ae32341ef6faca3c5976f171c3
MPL <marcin.labenski@gmail.com>
2024-08-15 01:10:33 +0200

    Unit tests updated - download/upload benchmarks added

--- mftTests/mftTests.swift
+++ mftTests/mftTests.swift
@@ -3,7 +3,7 @@
 //  mftTests
 //
 //  Created by Marcin Labenski on 28/01/2022.
-//  Copyright © 2022 Marcin Labenski. All rights reserved.
+//  Copyright © 2022-2024 Marcin Labenski. All rights reserved.
 //
 
 import XCTest
@@ -393,6 +393,10 @@ class mftTests: XCTestCase {
         NSLog("Cipher in: %@ out: %@", info.cipherIn, info.cipherOut)
         NSLog("HMAC in: %@ out: %@", info.hmacIn, info.hmacOut)
         NSLog("KEX alg: %@", info.kexAlg)
+        NSLog("Max Handlers: %d", info.maxOpenHandles);
+        NSLog("Max Packet Len: %d", info.maxPacketLenght);
+        NSLog("Max Read Len: %d", info.maxReadLenght);
+        NSLog("Max Write Len: %d", info.maxWriteLenght);
     }
     
     func testListWithLimit() throws {
@@ -515,5 +519,64 @@ class mftTests: XCTestCase {
         let attrs = try FileManager.default.attributesOfItem(atPath: testDest) as NSDictionary
         XCTAssert(attrs.fileSize() == 4000000)
         XCTAssert(md5(forFileAtPath: testSrc) == md5(forFileAtPath: testDest))
+    }
+    
+    func testBenchDownload() throws {
+        let srcTestFile = "/tmp/mft/src_download_test_big"
+        let destTestFile = "/tmp/mft/dest_download_test_big"
+        let data = NSMutableData()
+        
+        data.length = 1024 * 1024 * 1024
+        try data.write(toFile: srcTestFile)
+        
+        let outStream = OutputStream(toFileAtPath: destTestFile, append: false)
+        XCTAssert(outStream != nil)
+        
+        let start = DispatchTime.now().uptimeNanoseconds
+            
+        XCTAssertNoThrow(try sftp.contents(atPath: srcTestFile, toStream: outStream!, fromPosition: 0) {
+            downloaded, total in
+            return true
+        })
+        
+        let elapsed = Double(DispatchTime.now().uptimeNanoseconds - start) / 1_000_000_000
+        let rate = Double(data.length) / elapsed / (1024*1024)
+        NSLog("Download time: %d s, speed %f MB/s", Int(elapsed) as Int, rate)
+        XCTAssert(rate > 200.0)
+        
+        let srcAttrs = try FileManager.default.attributesOfItem(atPath: srcTestFile) as NSDictionary
+        let destAttrs = try FileManager.default.attributesOfItem(atPath: destTestFile) as NSDictionary
+        
+        XCTAssert(srcAttrs.fileSize() == destAttrs.fileSize())
+        XCTAssert(md5(forFileAtPath: srcTestFile) == md5(forFileAtPath: destTestFile))
+    }
+    
+    func testBenchUpload() throws {
+        let srcTestFile = "/tmp/mft/src_upload_test_big"
+        let destTestFile = "/tmp/mft/dest_upload_test_big"
+        let data = NSMutableData()
+        
+        data.length = 1024 * 1024 * 1024
+        try data.write(toFile: srcTestFile)
+        
+        let inStream = InputStream(fileAtPath: srcTestFile)
+        XCTAssert(inStream != nil)
+        
+        let srcAttrs = try FileManager.default.attributesOfItem(atPath: srcTestFile) as NSDictionary
+        
+        let start = DispatchTime.now().uptimeNanoseconds
+        
+        XCTAssertNoThrow(try sftp.write(stream: inStream!, toFileAtPath: destTestFile, append: false) { uploaded in
+            return true
+        })
+        
+        let elapsed = Double(DispatchTime.now().uptimeNanoseconds - start) / 1_000_000_000
+        let rate = Double(data.length) / elapsed / (1024*1024)
+        NSLog("Upload time: %d s, speed %f MB/s", Int(elapsed) as Int, rate)
+        XCTAssert(rate > 200.0)
+        
+        let destAttrs = try FileManager.default.attributesOfItem(atPath: destTestFile) as NSDictionary
+        XCTAssert(srcAttrs.fileSize() == destAttrs.fileSize())
+        XCTAssert(md5(forFileAtPath: destTestFile) == md5(forFileAtPath: srcTestFile))
     }
 }
